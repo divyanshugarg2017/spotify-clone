8f36c1a425955356ec16c49384d69d8bae2282aa
Divyanshu Garg <divyanshugarg1995@gmail.com>
2020-09-18 16:12:08 -0500

    fixed import error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useEffect } from 'react';
 import './App.css';
 import Login from './Login';
-import { getTokenFromUrl } from "./";
+import { getTokenFromResponse,  } from "./spotify";
 
 
 function App() {
 //useeffect helps in running code based on a given condition//
   useEffect(() => {
-    const token = getTokenFromUrl();
-    console.log("here is the token",token)
+    const token = getTokenFromResponse();
+    console.log("here is the token",token);
   }, []); 
 
 
