63179e939e5e9f7346a53e5c20cd19ce8454c79d
Divyanshu Garg <divyanshugarg1995@gmail.com>
2020-09-19 13:35:20 -0500

    setup communication witth spotify and player

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Login from './Login';
-import { getTokenFromResponse,  } from "./spotify";
+import { getTokenFromResponse } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js";
+import Player from "./Player"; 
+
+const spotify = new SpotifyWebApi();
 
 
 function App() {
+  const [token , setToken] = useState(null);
 //useeffect helps in running code based on a given condition//
-  useEffect(() => {
-    const token = getTokenFromResponse();
-    console.log("here is the token",token);
+useEffect(() => {
+  // Set token
+  const hash = getTokenFromResponse();
+  window.location.hash = "";
+  const _token = hash.access_token;
+
+//communicating react with spotify api and usin functions provided usin accesstoken //
+  if(_token){
+    setToken(_token)
+    spotify.setAccessToken(_token);
+
+    spotify.getMe().then(user => {
+      console.log('penguin',user);
+    });
+  }
+    
+    console.log("here is the token",_token);
   }, []); 
 
 
   return (
     <div className="app">
-      
-      <Login/>
+    {
+        token ? <Player/> : <Login/>
+    }  
     </div>
   );
-}
+      } 
+
 
 export default App;
